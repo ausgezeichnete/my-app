7a1005e2cfa0278dd7a84772b88d0948c181e733
Maha F. <ausgezeichnete@gmail.com>
2026-06-24 20:58:08 +0200

    created a new myBtn file

--- src/routes/layouts/appLayout/appLayout.tsx
+++ src/routes/layouts/appLayout/appLayout.tsx
@@ -1,11 +1,13 @@
 import { AppButton } from "@/common/appButton/appButton";
+import { myBtn } from "@/common/appButton/myBtn";
 
 export function AppLayout() {
-    return (
-        <>
-        {/* <div className="bg-red-300">asd</div> */}
-        {/* <div className="bg-slate-400 h-screen w-[30%] ml-auto">asd</div> */}
-        <AppButton buttonText="Hello" variant="contained" width="short" />
-        </>
-    )
+  return (
+    <>
+      {/* <div className="bg-red-300">asd</div> */}
+      {/* <div className="bg-slate-400 h-screen w-[30%] ml-auto">asd</div> */}
+      <AppButton buttonText="Hello" variant="contained" width="long" />
+      <myBtn text="click" variant="outlined" color="primary" />
+    </>
+  );
 }
