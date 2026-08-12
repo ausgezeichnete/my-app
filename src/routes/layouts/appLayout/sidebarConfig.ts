ffffbd6210f13aff174b1d5f5b2978eb0c96ee57
Maha F. <ausgezeichnete@gmail.com>
2026-08-12 15:25:26 +0200

    remove reports from sidebar

--- src/routes/layouts/appLayout/sidebarConfig.ts
+++ src/routes/layouts/appLayout/sidebarConfig.ts
@@ -19,10 +19,4 @@ export const items = [
     url: "/product-orders",
     icon: ShoppingCart,
   },
-  {
-    id: "reports",
-    title: "sidebar.reports",
-    url: "/reports",
-    icon: File,
-  },
 ];
