e440bbec445d2d2e370b93697e4d9cf3f18ac3ff
Maha F. <ausgezeichnete@gmail.com>
2026-08-12 15:03:39 +0200

    remove shipment from development base

--- src/routes/layouts/Router.tsx
+++ src/routes/layouts/Router.tsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { AppLayout } from "./appLayout/appLayout";
 import { Clients } from "@/features/clients/clients";
 import { ProductOrder } from "@/features/productOrder/productOrder";
-import { Shipment } from "@/features/shipment/shipment";
+//import { Shipment } from "@/features/shipment/shipment";
 
 export const AppRouter = () => {
   return (
@@ -10,7 +10,6 @@ export const AppRouter = () => {
       <Route path="/" element={<AppLayout />}>
         <Route path="clients" element={<Clients />} />
         <Route path="product-orders" element={<ProductOrder />} />
-        <Route path="shipments" element={<Shipment />} />
       </Route>
     </Routes>
   );
