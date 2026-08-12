20bddf9bf48c49689aaec980ed832359bed965de
Maha F. <ausgezeichnete@gmail.com>
2026-08-12 15:08:04 +0200

    shipment route added

--- src/routes/layouts/Router.tsx
+++ src/routes/layouts/Router.tsx
@@ -1,15 +1,17 @@
-import { Route, Routes } from "react-router-dom"
-import { AppLayout } from "./appLayout/appLayout"
-import { Clients } from "@/features/clients/clients"
-import { ProductOrder } from "@/features/productOrder/productOrder"
+import { Route, Routes } from "react-router-dom";
+import { AppLayout } from "./appLayout/appLayout";
+import { Clients } from "@/features/clients/clients";
+import { ProductOrder } from "@/features/productOrder/productOrder";
+import { Shipment } from "@/features/shipment/shipment";
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<AppLayout />}>
-        <Route path="clients" element={<Clients />}/>
-        <Route path="product-orders" element={<ProductOrder />}/>
+        <Route path="clients" element={<Clients />} />
+        <Route path="product-orders" element={<ProductOrder />} />
+        <Route path="shipments" element={<Shipment />} />
       </Route>
     </Routes>
-  )
-}
+  );
+};
